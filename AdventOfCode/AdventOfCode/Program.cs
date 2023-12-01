6342e202ae6aa77769e6b9be0007912b459f73ad
padjal <padjal136@gmail.com>
2023-12-01 15:39:57 +0200

    day 1 solution

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace AdventOfCode
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Task1;
+
+namespace AdventOfCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string line;
+            int sum = 0;
+
+            while(!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+
+                var testNumberRegex = new Regex("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|[0-9]");
+                var testNumberRegexFromRight = new Regex("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|[0-9]", RegexOptions.RightToLeft);
+
+                var leftNumber = testNumberRegex.Match(line).Value;
+                var rightNumber = testNumberRegexFromRight.Match(line).Value;
+
+                if (leftNumber.Length != 1) leftNumber = leftNumber.ToDigit();
+                if (rightNumber.Length != 1) rightNumber = rightNumber.ToDigit();
+
+                int.TryParse(leftNumber
+                    + rightNumber, out int result);
+
+                Console.WriteLine(result);
+
+                sum += result;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
---
+++ AdventOfCode/TestCases/Task1/WrittenToDigitTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace TestCases.Task1
+{
+    [TestClass]
+    public class WrittenToDigitTests
+    {
+        private Regex regex = new Regex("(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|[0-9]");
+
+        [TestMethod]
+        [DataRow("one")]
+        [DataRow("two")]
+        [DataRow("three")]
+        [DataRow("four")]
+        [DataRow("five")]
+        [DataRow("six")]
+        [DataRow("seven")]
+        [DataRow("eight")]
+        [DataRow("fdsdonenine")]
+        [DataRow("sdfsdftwonine")]
+        [DataRow("3324nine")]
+        [DataRow("xxnine")]
+        [DataRow("%$#%nine")]
+        [DataRow("sevenine")]
+        [DataRow("1s;ldftwo")]
+        public void FindMatchInBeginning(string input)
+        {
+            var matches = regex.Matches(input);
+
+            Assert.IsTrue(matches.FirstOrDefault().Success);
+
+            Console.WriteLine($"Found match {matches.FirstOrDefault().Value} in {input}.");
+        }
+
+        [TestMethod]
+        [DataRow("one")]
+        [DataRow("two")]
+        [DataRow("three")]
+        [DataRow("four")]
+        [DataRow("five")]
+        [DataRow("six")]
+        [DataRow("seven")]
+        [DataRow("eight")]
+        [DataRow("nine")]
+        [DataRow("sevenine")]
+        public void FindMatchInEnd(string input)
+        {
+            var matches = regex.Matches(input);
+
+            Assert.IsTrue(matches.LastOrDefault().Success);
+
+            Console.WriteLine($"Found match {matches.LastOrDefault().Value} in {input}.");
+        }
+
+        [TestMethod]
+        public void FindEdgeCases()
+        {
+            var input = "sevenine";
+
+            var matches = regex.Matches(input);
+
+            Assert.IsTrue(matches.LastOrDefault().Success);
+            Assert.IsTrue(matches.LastOrDefault().Value == "nine");
+
+            Console.WriteLine($"Found match {matches.LastOrDefault().Value} in {input}.");
+        }
+    }
+}

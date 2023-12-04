ab62a640dffffcceeda80f25e7a574e70799a035
padjal <padjal136@gmail.com>
2023-12-04 13:24:25 +0200

    Solution day 4

---
+++ AdventOfCode/AdventOfCode.Core/Helpers/InputParser.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace AdventOfCode.Core.Helpers
+{
+    public static class InputParser
+    {
+        public static string[] GetLines(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        public static char[][] GetMatrix(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+
+            var matrix = new char[lines.Length][];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                matrix[i] = lines[i].ToCharArray();
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Core/Interfaces/TaskBase.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Core.Interfaces
+{
+    public interface ITask
+    {
+        void PartOne();
+
+        void PartTwo();
+    }
+}

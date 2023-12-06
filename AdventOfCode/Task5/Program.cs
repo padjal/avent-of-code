bb24bc5806222ceda86779835f751dcf81f9e2b5
padjal <padjal136@gmail.com>
2023-12-06 15:41:57 +0200

    Update readme

---
+++ AdventOfCode/Task5/Program.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Core.Interfaces;
+
+namespace Task5
+{
+    internal class Program
+    {
+        const string INPUT_FILE1 = "../../../input1.txt";
+        const string INPUT_FILE2 = "../../../input2.txt";
+
+        static void Main(string[] args)
+        {
+            ParseInput();
+        }
+
+        static void ParseInput()
+        {
+            var input = File.ReadAllLines(INPUT_FILE1);
+
+            var seeds = input[0].Split(':')[1].Trim().Split(' ').Select(n => int.Parse(n)).ToList();
+        }
+
+        public void PartOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PartTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+        class RangeMap
+        {
+            public int DestinationStart { get; set; }
+
+            public int SourceStart { get; set; }
+
+            public int RangeLength { get; set; }
+        }
+
+        class Mapping
+        {
+            public List<RangeMap> RangeMaps { get; set; } = new List<RangeMap>();
+
+            public string Name { get; set; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # avent-of-code
 Repo for solving the Avent of Coding 2023 challenge
+
+# Input files
+As per the Advent of Code official guidelines, question prompts as well as input data should not be copied nor distributed. It is why I have ommited the input files, which are part of this repository in the github public version.

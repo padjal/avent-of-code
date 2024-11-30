2862710747600a043410f2821ac1268bc0d656c7
padjal <padjal136@gmail.com>
2024-12-01 00:05:23 +0200

    sync latest changes

---
+++ AdventOfCode/Task9/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task9
+{
+    internal class Program
+    {
+        const string INPUT_FILE = "../../../input.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static List<int>[] ParseInput()
+        {
+            var input = File.ReadLines(INPUT_FILE);
+
+            var values = new List<int>[input.Count()];
+
+            foreach (var line in input)
+            {
+                var numbers = line.Split(' ').Select(n => int.Parse(n));
+            }
+
+            return values;
+        }
+
+        static int GetPrediction(IEnumerable<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+
+            }
+
+            return 0;
+        }
+    }
+}
